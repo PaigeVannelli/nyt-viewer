d5b58973092c6ee154e6621e153ce524b38864b9
Paige Vannelli <paigevannelli@gmail.com>
2021-06-01 14:02:51 -0600

    Inital commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+const App = () => {
+  return (
+    //main page 
+    //detailed article view 
+    //404 not found 
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
