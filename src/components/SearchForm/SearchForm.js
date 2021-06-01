09aa718326129e0f06ba37ac9b9625f9c16c3594
Paige Vannelli <paigevannelli@gmail.com>
2021-06-01 15:17:12 -0600

    Add sections to search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,15 +7,26 @@ const SearchForm = () => {
         <option value='automobiles'>Automobiles</option>
         <option value='books'>Books</option>
         <option value='business'>Business</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value=''>Fiat</option>
-        <option value="audi">Audi</option>
+        <option value='fashion'>Fashion</option>
+        <option value='food'>Food</option>
+        <option value='health'>Health</option>
+        <option value='home'>Home</option>
+        <option value='insider'>Insider</option>
+        <option value='magazine'>Magazine</option>
+        <option value='movies'>Movies</option>
+        <option value='nyregion'>NY Region</option>
+        <option value='obituaries'>Obituaries</option>
+        <option value='opinion'>Opinion</option>
+        <option value='politics'>Politics</option>
+        <option value='realestate'>Real Estate</option>
+        <option value='science'>Science</option>
+        <option value='sundayreview'>Sunday Review</option>
+        <option value='techology'>Technology</option>
+        <option value='t-magazine'>T-Magazine</option>
+        <option value='travel'>Travel</option>
+        <option value='upshot'>Upshot</option>
+        <option value='us'>US</option>
+        <option value='world'>World</option>
       </select>
     </form>
   )
