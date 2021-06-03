f57ecbc6b405563e31c168236762b4c34099d451
Paige Vannelli <paigevannelli@gmail.com>
2021-06-02 18:58:39 -0600

    Add box styling to main articles

--- src/components/ArticleOverview/ArticleOverview.js
+++ src/components/ArticleOverview/ArticleOverview.js
@@ -1,18 +1,15 @@
 import { Link } from 'react-router-dom'
+import './ArticleOverview.css'
 
 // const ArticleOverview = ({ key, title, abstract, url, byline, date, background, thumbnail, setCurrentArticle }) => {
 const ArticleOverview = (props) => {
   return (
-    <Link to='/article'>
-      <article onClick={() => props.setCurrentArticle(props)}>
-        <img src={props.thumbnail} />
-        <div>
-          <h1>{props.title}</h1>
-          <p>{props.byline}</p>
-          <p>{props.abstract}</p>
-          <p>{props.date}</p>
-        </div>
-      </article>
+    <Link to='/article' onClick={() => props.setCurrentArticle(props)} className='article-overview'>
+      <img src={props.thumbnail} />
+      <div>
+        <p>{props.title}</p>
+        <p>{props.byline}</p>
+      </div>
     </Link>
   )
 }
