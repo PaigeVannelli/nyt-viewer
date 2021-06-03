1fabf628710d132ebf6c33f8a08d1dbd0b36d57d
Paige Vannelli <paigevannelli@gmail.com>
2021-06-03 08:41:29 -0600

    Set image as background

--- src/components/ArticleOverview/ArticleOverview.js
+++ src/components/ArticleOverview/ArticleOverview.js
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom'
 import './ArticleOverview.css'
 
-// const ArticleOverview = ({ key, title, abstract, url, byline, date, background, thumbnail, setCurrentArticle }) => {
 const ArticleOverview = (props) => {
   return (
-    <Link to='/article' onClick={() => props.setCurrentArticle(props)} className='article-overview'>
-      <img src={props.thumbnail} alt={props.title} className='thumbnail'/>
-      <div>
+    <Link to='/article' onClick={() => props.setCurrentArticle(props)} className='article-overview' style={{backgroundImage: `url(${props.thumbnail})`}}>
+      {/* <img src={props.thumbnail} alt={props.title} className='thumbnail'/> */}
+      <div className='details'>
         <p>{props.title}</p>
         <p>{props.byline}</p>
       </div>
